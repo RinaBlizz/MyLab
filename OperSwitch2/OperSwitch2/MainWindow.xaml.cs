dd0652a9d3b77ac4f3dbb55efd82320315b28dad
RinaBlizz <74242070+RinaBlizz@users.noreply.github.com>
2020-11-10 15:31:16 +0200

    Add files via upload

---
+++ OperSwitch2/OperSwitch2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OperSwitch2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                byte n = byte.Parse(Kopeck.Text);
+                if (n == 1 || n % 10 == 1 && n != 11)
+                    Result.Content = "копейка";
+                else 
+                if (n % 10 >= 2 && n % 10 <= 4 && n != 12 && n != 13 && n != 14)
+                    Result.Content = "копейки";
+                else Result.Content = "копеек";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
